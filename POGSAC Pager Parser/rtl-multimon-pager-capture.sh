7960f665b6b7b0d8e673a43a57321f64d5d0f356
Nathan Barrow <78792177+barrown@users.noreply.github.com>
2026-01-06 09:18:15 +0000

    Delete POGSAC Pager Parser/rtl-multimon-pager-capture.sh

--- POGSAC Pager Parser/rtl-multimon-pager-capture.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/bin/rtl_fm -f 153340000 -s 22050 | /usr/local/bin/multimon-ng -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -f alpha --timestamp /dev/stdin | /bin/python3 /home/pi/pagers/pager_pipe_parser.py
